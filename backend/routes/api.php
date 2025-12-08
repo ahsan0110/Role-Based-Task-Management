17cce7857d36c17dc9899e88723f28e44a74a55f
Muhammad Ahsan <muhammadahsan1404@gmail.com>
2025-12-08 19:58:00 +0500

    Fixed api.php routes

---
+++ backend/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group
+| which is assigned the "api" middleware group.
+|
+*/
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working']);
+});
